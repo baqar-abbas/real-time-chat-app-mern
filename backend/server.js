a8e24ebc34489336476c9606264a4de88e23e81b
baqar-abbas <baqar007@hotmail.com>
2024-04-26 19:06:40 +0500

    Create DB Project on Mongo DB Atlas and connect to DB through connection string

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js';
+import { connect } from 'mongoose';
+import connectToMongoDb from './db/connectToMongoDb.js';
 
 const app = express();
 dotenv.config();
@@ -14,5 +16,6 @@ app.get('/', (req, res) => {
 app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
+    connectToMongoDb();
     console.log(`Server is running on port ${PORT}`);
 })
