0dbcde873b33fd063bf8fc73a80dcd514d8fd575
baqar-abbas <baqar007@hotmail.com>
2024-04-27 01:15:25 +0500

    Create User Schema, User Model and update the signup controller to handle POST request

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,56 @@
-export const login = (req, res) => {
-    res.send('Login Route');
-    console.log('Login user');
+import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs';
+
+export const signup = async (req, res) => {
+  try {
+  const { fullName, username, password, confirmPassword, gender } = req.body;
+
+  // Check if Password and Confirm Password are same
+  if(password !== confirmPassword) {
+    return res.status(400).json({ error: "Password and Confirm Password do not match" });
+  }
+
+  const user = await User.findOne({username});
+  // Check if the user already exists
+    if(user) {
+        return res.status(400).json({ error: "User already exists" });
+    }
+
+  // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    // Create a new user
+    const newUser = new User({
+        fullName,
+        username,
+        password: hashedPassword,
+        gender,
+        profilePic: gender === 'male' ? boyProfilePic : girlProfilePic
+    });
+
+    if(newUser) {
+
+    await newUser.save();
+
+    res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        gender: newUser.gender,
+        profilePic: newUser.profilePic
+    })
+} else {
+    res.status(400).json({ error: "Invalid User Data" });
+}
+  
+  } catch (error) {
+    console.log("There is an error in signup route", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
 
 export const logout = (req, res) => {
@@ -8,7 +58,7 @@ export const logout = (req, res) => {
     console.log('Logout user');
 }
 
-export const signup = (req, res) => {
+export const login = (req, res) => {
     res.send('Sign Up Route');
     console.log('Sign up user');
 }
--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,15 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World chat app');
-});
+app.use(express.json()); // to parse the incoming request with JSON payloads (from req.body)
 
 app.use('/api/auth', authRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Hello from Express');
+})
+
+// Listen to the requests and connect to Mongo DB Atlas cluster
 app.listen(PORT, () => {
     connectToMongoDb();
     console.log(`Server is running on port ${PORT}`);
